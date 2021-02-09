3b2fc91441446d8659bd160fda9c0524c07f5bfb
carlosmgc2003 <carlosmgc2003@gmail.com>
2021-02-09 11:00:07 -0300

    Eliminado el initial setup de influx-db en powershell

--- influxdb-initialsetup.ps1
+++
@@ -1,7 +0,0 @@
-docker exec metrics-db sh -c "influx setup --org ccic --bucket mensajes --username dashboard --password ExAmPl3PA55W0rD --token u8C0TQ4SOAQ6za2BLZO-grxyIg5NT-WEQGNE9O6pYy73rPt9t0MNx8trbAgXEdyrwXYQ9dCRs5-zr3BMeeVufw== --force"
-docker exec metrics-db sh -c "influx bucket create --name cola-cmd --retention 48h"
-docker exec metrics-db sh -c "influx bucket create --name mensajes-ccic --retention 48h"
-docker exec metrics-db sh -c "influx bucket create --name gpos-rtef --retention 48h"
-docker exec metrics-db sh -c "influx bucket create --name estado-servicio --retention 48h"
-docker exec metrics-db sh -c "influx bucket create --name combustible-generador --retention 48h"
-docker exec metrics-db sh -c "influx user create --name jccic --password ExAmPl3PA55W0rD"
