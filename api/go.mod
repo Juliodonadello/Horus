071bd5e48a70bea75028295e63e550c8fc5ee251
carlosmgc2003 <carlosmgc2003@gmail.com>
2021-02-19 03:42:45 -0300

    Implementado gorutina con servidor con TLS activado

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module api
 go 1.15
 
 require (
+	github.com/gin-contrib/secure v0.0.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"api/event_db"
 	"api/metrics_db"
 	"api/server"
+	"fmt"
 	"time"
 )
 
@@ -11,7 +12,10 @@ import (
 // https://github.com/vsouza/go-gin-boilerplate
 func main() {
 	time.Sleep(10 * time.Second)
-	s := server.Init()
+	currentTime := time.Now()
+	fmt.Println("Current Time in String: ", currentTime.String())
+	httpsRouter := server.NewRouter()
+	httpRouter := server.NewRouter()
 	err := metrics_db.Init()
 	if err != nil {
 		panic("No se pudo conectar con Metricas-DB")
@@ -20,5 +24,6 @@ func main() {
 	if err != nil {
 		panic("No se pudo conectar con Event-DB")
 	}
-	_ = s.ListenAndServe() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	go httpRouter.Run(":80")
+	httpsRouter.RunTLS(":443", "localhost.crt", "localhost.key")
 }
