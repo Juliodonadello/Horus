6382e9eb520cb58ff280baae05c256d7a4e241ab
carlosmgc2003 <carlosmgc2003@gmail.com>
2021-02-23 13:52:25 -0300

    Corregido bug que impedia ejecutar el bash script creado en windows por los finales de linea '/r'

--- tsdb/influx-setup.sh
+++ tsdb/influx-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 influxd & disown
 
-sleep 10
+sleep 20
 influx config
 influx setup --org ccic --bucket ccic-inicial --username dashboard --password ExAmPl3PA55W0rD --token u8C0TQ4SOAQ6za2BLZO-grxyIg5NT-WEQGNE9O6pYy73rPt9t0MNx8trbAgXEdyrwXYQ9dCRs5-zr3BMeeVufw== --force
 influx bucket create --name colas-espera --retention 48h
